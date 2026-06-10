a4e908aa70301ff0dd56af1242d2c5a1fba4d581
Sarah-Sou <sarah.soueidan@ontariotechu.net>
2026-06-09 20:17:33 -0400

    Added CSS styling and static file support

--- server/server.js
+++ server/server.js
@@ -3,12 +3,17 @@ const path = require("path");
 
 const app = express();
 
+// Serve static files (CSS, JS, Images)
+app.use(express.static("public"));
+
 const PORT = 3000;
 
+// Homepage Route
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
+// Start Server
 app.listen(PORT, () => {
     console.log(`CyberWatch running on port ${PORT}`);
 });
