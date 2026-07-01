9df2f7b16aacce5fc348b1b3169f688714bb0202
Sarah-Sou <sarah.soueidan@ontariotechu.net>
2026-06-30 20:11:04 -0400

    Complete Week 3 Day 1 with login page and dashboard improvements

--- server/server.js
+++ server/server.js
@@ -3,14 +3,19 @@ const path = require("path");
 
 const app = express();
 
-// Serve static files (CSS, JS, Images)
+// Serve static files (CSS, JavaScript, Images)
 app.use(express.static("public"));
 
 const PORT = 3000;
 
-// Homepage Route
+// Dashboard (Homepage)
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../views/index.html"));
+});
+
+// Login Page
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "../views/login.html"));
 });
 
 // Start Server
